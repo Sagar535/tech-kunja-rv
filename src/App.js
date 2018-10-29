48b63dc105ce5eb8a5352907e87a37e7cd593229
Sagar Shah <070bct535@ioe.edu.np>
2018-10-29 15:40:55 +0545

    Now I can see the csv file in table - NOTE: never forget to import react-virtualized/styles.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Table, Column} from 'react-virtualized';
+import CSVReader from 'react-csv-reader';
 import './App.css';
+import 'react-virtualized/styles.css'
+
+let globalData=['gag'];
 
 class App extends Component {
+  state = {
+    data: globalData
+  }
+
+  displayData = (data) => {
+    globalData = data;
+    console.log('The new globalData', globalData);
+    this.setState({
+      data: globalData
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <CSVReader 
+            label="select the csv file you want to display"
+            onFileLoaded={this.displayData}
+          />
         </header>
+
+        <div>
+          <Table
+            width={500}
+            height={300}
+            rowHeight={30}
+            rowCount={globalData.length}
+            rowGetter={({index}) => globalData[index]}
+          >
+            <Column 
+              width={100}
+              dataKey="0"
+            />
+            <Column 
+              width={100}
+              dataKey="1"
+            />
+            <Column 
+              width={100}
+              dataKey="2"
+            />
+            <Column 
+              width={100}
+              dataKey="3"
+            />
+            
+          </Table>
+        </div>
       </div>
     );
   }
